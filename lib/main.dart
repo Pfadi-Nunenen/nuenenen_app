76321485fead69baba76fa25fc15352dec04bfaa
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2019-12-28 00:06:08 +0100

    Test different Push-Method

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,97 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class PushMessaging extends StatefulWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  _PushMessagingState createState() => _PushMessagingState();
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class _PushMessagingState extends State<PushMessaging> {
-  String _homeScreenText = "Waiting for token...";
-  String _messageText = "Waiting for message...";
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+  String message = 'Firebase message will show up here';
 
   @override
   void initState() {
     super.initState();
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
-        setState(() {
-          _messageText = "Push Messaging message: $message";
-        });
-        print("onMessage: $message");
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        setState(() {
-          _messageText = "Push Messaging message: $message";
-        });
-        print("onLaunch: $message");
+        print('on message $message');
+        _setMessageText(message.toString());
       },
       onResume: (Map<String, dynamic> message) async {
-        setState(() {
-          _messageText = "Push Messaging message: $message";
-        });
-        print("onResume: $message");
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
       },
     );
+
     _firebaseMessaging.requestNotificationPermissions(
         const IosNotificationSettings(sound: true, badge: true, alert: true));
-    _firebaseMessaging.onIosSettingsRegistered
-        .listen((IosNotificationSettings settings) {
-      print("Settings registered: $settings");
+
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
     });
-    _firebaseMessaging.getToken().then((String token) {
-      assert(token != null);
-      setState(() {
-        _homeScreenText = "Push Messaging token: $token";
-      });
-      print(_homeScreenText);
+  }
+
+  _setMessageText(String text) {
+    setState(() {
+      message = text;
     });
+  }
+
+  _resetText() {
+    _setMessageText('Firebase message will show up here');
+  }
+
+  _bodyWidget() {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              message,
+              style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Pfadi Nünenen'),
-        ),
-        drawer: Drawer(
-
-        ),
-        body: Material(
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: Text(_homeScreenText),
-              ),
-              Row(children: <Widget>[
-                Expanded(
-                  child: Text(_messageText),
-                ),
-              ])
-            ],
-          ),
-        ));
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: _bodyWidget(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          _resetText();
+        },
+      ),
+    );
   }
-}
-
-void main() {
-  runApp(
-    MaterialApp(
-      home: PushMessaging(),
-    ),
-  );
 }
