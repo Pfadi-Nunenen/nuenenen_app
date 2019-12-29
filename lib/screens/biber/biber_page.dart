528ad52ed8d99ee61a8a1944d58314da88f8df77
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2019-12-29 18:46:14 +0100

    Complete Biberpage

--- lib/screens/biber/biber_page.dart
+++ lib/screens/biber/biber_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:nuenenen/theme/colors.dart';
 import 'package:nuenenen/models/kastenzettel.dart';
 import 'package:nuenenen/user_info.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BiberPage extends StatefulWidget{
 	@override
@@ -16,6 +17,7 @@ class BiberPage extends StatefulWidget{
 
 class _BiberPage extends State<BiberPage>{
 	List<Kastenzettel> kastenzettelList = new List();
+	var isLoading = false;
 
 	@override
 	void initState(){
@@ -24,6 +26,10 @@ class _BiberPage extends State<BiberPage>{
 	}
 
 	Future onRefresh() async{
+		setState(() {
+		  isLoading = true;
+		});
+
 		print("Lade Daten für den Kastenzettel herunter");
 		kastenzettelList.clear();
 		var URL = "${baseURI}/node/${biberID}/json";
@@ -31,11 +37,18 @@ class _BiberPage extends State<BiberPage>{
 		try{
 			await http.get(URL).then((response){
 				var json = jsonDecode(response.body);
-				setState(() {
-					kastenzettelList.add(new Kastenzettel(json['title'].toString(), json['body']['und'][0]['value'].toString()));
-				});
+				for(int i = 0; i < json.length; i++) {
+					setState(() {
+						kastenzettelList.add(new Kastenzettel(
+								json['title'].toString(),
+								json['body']['und'][0]['value'].toString()
+						));
+					});
+				}
 			});
-			setState(() {});
+			setState(() {
+				isLoading = false;
+			});
 		}catch(error){
 			print("Failed to get Kastenzettel - $error");
 		}
@@ -43,36 +56,51 @@ class _BiberPage extends State<BiberPage>{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-	  return Scaffold(
-		  body: RefreshIndicator(
+	  return new Scaffold(
+		  body: isLoading ?
+				  new Container(
+					  color: currBackgroundColor,
+						child: Center(
+							child: CircularProgressIndicator(),
+						)
+				  )
+			: new RefreshIndicator(
 			  onRefresh: onRefresh,
 			  color: darkTextColor,
-			  backgroundColor: mainColor,
+			  backgroundColor: currBackgroundColor,
 			  child: new Container(
-					color: currBackgroundColor,
+				  color: currBackgroundColor,
 				  padding: EdgeInsets.all(16.0),
 				  child: new Container(
 					  child: new Column(
 						  children: <Widget>[
-						  	new Text(
-								  "${kastenzettelList[0].title}",
-								  style: new TextStyle(
-									  color: darkTextColor,
-									  fontSize: 30,
-									  fontWeight: FontWeight.bold,
+							  new Text(
+								  kastenzettelList[0].title.toString(),
+								  style: TextStyle(
+										  color: currTextColor,
+										  fontSize: 25.0,
+										  fontWeight: FontWeight.bold
 								  ),
 							  ),
 							  new Html(
-								  data: "${kastenzettelList[0].content}",
+								  data: kastenzettelList[0].content,
 								  renderNewlines: true,
-							  )
+								  onLinkTap: (url) async {
+									  if (await canLaunch(url)) {
+										  await launch(url, forceSafariVC: false);
+									  } else {
+										  throw 'Could not launch $url';
+									  }
+								  },
+								  defaultTextStyle: TextStyle(
+										  color: currTextColor
+								  ),
+							  ),
 						  ],
 					  ),
 				  ),
 			  ),
-		  )
+		  ),
 	  );
   }
-
 }
