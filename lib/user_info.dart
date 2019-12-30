71cb10990f03cf17c76e0ffbaa4882939d9bbda7
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2019-12-30 17:38:28 +0100

    Update versions & add notification-codes

--- lib/user_info.dart
+++ lib/user_info.dart
@@ -2,14 +2,14 @@ import 'package:fluro/fluro.dart';
 
 final router = Router();
 
-double appVersion = 2.0;
-int appBuild = 4;
+double appVersion = 3.0;
+int appBuild = 9;
 String appStatus = "";
 String appFull = "Version $appVersion";
 
 int lastPage = 0;
 bool offlineMode = false;
-bool darkMode = false;
+bool darkMode = true;
 
 String baseURI = "https://pfadi-nuenenen.ch";
 
@@ -17,3 +17,8 @@ String biberID = "214";
 String wolfID = "215";
 String aetnaID = "203";
 String saturnID = "213";
+
+bool biberNotification = false;
+bool wolfNotification = false;
+bool aetnaNotification = false;
+bool saturnNotification = false;
