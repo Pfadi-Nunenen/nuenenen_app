ac4b9deadc26e213c79477872656df713d02fffc
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2020-02-11 19:32:01 +0100

    Cleanup import

--- lib/screens/overview/stufen_page.dart
+++ lib/screens/overview/stufen_page.dart
@@ -1,7 +1,6 @@
 import 'package:fluro/fluro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:nuenenen/theme/colors.dart';
 import 'package:nuenenen/user_info.dart';
 
