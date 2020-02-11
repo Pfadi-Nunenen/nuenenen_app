973e58f621a7b82f8683b8ca869f9d790cf44488
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-02-11 19:50:21 +0100

    Cleanup test-data

--- lib/screens/tools/qr_page.dart
+++ lib/screens/tools/qr_page.dart
@@ -1,9 +1,4 @@
-import 'dart:async';
-
-import 'package:nuenenen/api/FireStorage.dart';
-import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class QRReader extends StatefulWidget {
   @override
@@ -35,7 +30,6 @@ class _QRReader extends State<QRReader> {
                     color: Colors.blue,
                     textColor: Colors.white,
                     splashColor: Colors.blueGrey,
-                    onPressed: scan,
                     child: const Text('START CAMERA SCAN')
                 ),
               )
@@ -48,24 +42,5 @@ class _QRReader extends State<QRReader> {
             ],
           ),
         ));
-  }
-
-  Future scan() async {
-    try {
-      String barcode = await BarcodeScanner.scan();
-      setState(() => this.barcode = barcode);
-    } on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() {
-          this.barcode = 'The user did not grant the camera permission!';
-        });
-      } else {
-        setState(() => this.barcode = 'Unknown error: $e');
-      }
-    } on FormatException{
-      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
-    } catch (e) {
-      setState(() => this.barcode = 'Unknown error: $e');
-    }
   }
 }
