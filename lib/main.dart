22150d5919230949c611c32bc1436ebbf87a6fe5
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-10-23 21:40:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,64 +3,68 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:nuenenen/screens/kastenzettel/biber_page.dart';
+import 'package:nuenenen/screens/kastenzettel/wolf_page.dart';
 import 'package:nuenenen/screens/kastenzettel/aetna_page.dart';
 import 'package:nuenenen/screens/kastenzettel/saturn_page.dart';
+import 'package:nuenenen/screens/overview/stufen_page.dart';
 import 'package:nuenenen/screens/settings/about_page.dart';
-import 'package:nuenenen/screens/settings/help_page.dart';
-import 'package:nuenenen/screens/kastenzettel/wolf_page.dart';
+import 'package:nuenenen/screens/tools/qr_page.dart';
 import 'package:nuenenen/tab_bar_controller.dart';
 import 'package:nuenenen/theme/theme.dart';
 import 'package:nuenenen/user_info.dart';
-import 'package:nuenenen/screens/tools/qr_page.dart';
 
-void main(){
+void main() {
   // Home Routes
-  router.define('/home', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new TabBarController();
+  router.define('/home', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return TabBarController();
   }));
 
   // Settings Routes
-  router.define('/about', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new AboutPage();
+  router.define('/about', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return AboutPage();
   }));
 
-  router.define('/help', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new HelpPage();
+  router.define('/stufen', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return StufenPage();
   }));
 
-  router.define('/stufen', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new HelpPage();
+  router.define('/biber', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return BiberPage();
   }));
 
-  router.define('/biber', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new BiberPage();
+  router.define('/wolf', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return WolfPage();
   }));
 
-  router.define('/wolf', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new WolfPage();
+  router.define('/aetna', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return AetnaPage();
   }));
 
-  router.define('/aetna', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new AetnaPage();
+  router.define('/saturn', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return SaturnPage();
   }));
 
-  router.define('/saturn', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new SaturnPage();
+  router.define('/qr-reader', handler:
+      Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+    return QRReader();
   }));
 
-  router.define('/qr-reader', handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-    return new QRReader();
-  }));
-
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
     title: "Pfadi Nünenen",
-    home: new TabBarController(),
+    home: TabBarController(),
     onGenerateRoute: router.generator,
     debugShowCheckedModeBanner: false,
     theme: mainTheme,
   ));
 
   // Subscribe all devices to a general notification chanel
-  FirebaseMessaging _firebaseMessaging =  FirebaseMessaging();
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   _firebaseMessaging.subscribeToTopic('any');
 }
