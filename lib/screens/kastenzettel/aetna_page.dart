98d8e8b09e07acc1d74cc8b8228c96775595bd15
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-10-23 20:44:49 +0200

    Update aetna_page.dart

--- lib/screens/kastenzettel/aetna_page.dart
+++ lib/screens/kastenzettel/aetna_page.dart
@@ -8,22 +8,22 @@ import 'package:nuenenen/models/kastenzettel.dart';
 import 'package:nuenenen/user_info.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class AetnaPage extends StatefulWidget{
+class AetnaPage extends StatefulWidget {
   @override
   _AetnaPage createState() => _AetnaPage();
 }
 
-class _AetnaPage extends State<AetnaPage>{
-  List<Kastenzettel> kastenzettelList = new List();
+class _AetnaPage extends State<AetnaPage> {
+  List<Kastenzettel> kastenzettelList = List();
   var isLoading = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     onRefresh();
   }
 
-  Future onRefresh() async{
+  Future onRefresh() async {
     setState(() {
       isLoading = true;
     });
@@ -32,22 +32,23 @@ class _AetnaPage extends State<AetnaPage>{
     kastenzettelList.clear();
     var URL = "${baseURI}/node/${aetnaID}/json";
 
-    try{
-      await http.get(URL).then((response){
+    try {
+      await http.get(URL).then((response) {
         var json = jsonDecode(response.body);
-        for(int i = 0; i < json.length; i++) {
+        for (int i = 0; i < json.length; i++) {
           setState(() {
-            kastenzettelList.add(new Kastenzettel(
-                json['title'].toString(),
-                json['body']['und'][0]['value'].toString()
+            kastenzettelList.add(Kastenzettel(
+              json['title'].toString(),
+              json['body']['und'][0]['value'].toString()
             ));
           });
         }
       });
+      
       setState(() {
         isLoading = false;
       });
-    }catch(error){
+    } catch (error) {
       print("Failed to get Kastenzettel - $error");
     }
   }
@@ -59,57 +60,51 @@ class _AetnaPage extends State<AetnaPage>{
         backgroundColor: mainColor,
         actionsForegroundColor: Colors.white,
         previousPageTitle: "Stufen",
-        middle: new Text(
+        middle: Text(
           "Aetna",
-          style: TextStyle(
-              color: Colors.white
-          ),
+          style: TextStyle(color: Colors.white),
         ),
       ),
-      body: isLoading ?
-      new Container(
-          color: currBackgroundColor,
-          child: Center(
-            child: CircularProgressIndicator(),
-          )
-      )
-          : new RefreshIndicator(
-        onRefresh: onRefresh,
-        color: darkTextColor,
-        backgroundColor: currBackgroundColor,
-        child: new Container(
-          color: currBackgroundColor,
-          padding: EdgeInsets.all(16.0),
-          child: new Container(
-            child: new Column(
-              children: <Widget>[
-                new Text(
-                  kastenzettelList[0].title.toString(),
-                  style: TextStyle(
-                      color: currTextColor,
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold
+      body: isLoading
+          ? Container(
+              color: currBackgroundColor,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ))
+          : RefreshIndicator(
+              onRefresh: onRefresh,
+              color: darkTextColor,
+              backgroundColor: currBackgroundColor,
+              child: Container(
+                color: currBackgroundColor,
+                padding: EdgeInsets.all(16.0),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        kastenzettelList[0].title.toString(),
+                        style: TextStyle(
+                            color: currTextColor,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Html(
+                        data: kastenzettelList[0].content,
+                        renderNewlines: true,
+                        onLinkTap: (url) async {
+                          if (await canLaunch(url)) {
+                            await launch(url, forceSafariVC: false);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        },
+                        defaultTextStyle: TextStyle(color: currTextColor),
+                      ),
+                    ],
                   ),
                 ),
-                new Html(
-                  data: kastenzettelList[0].content,
-                  renderNewlines: true,
-                  onLinkTap: (url) async {
-                    if (await canLaunch(url)) {
-                      await launch(url, forceSafariVC: false);
-                    } else {
-                      throw 'Could not launch $url';
-                    }
-                  },
-                  defaultTextStyle: TextStyle(
-                      color: currTextColor
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
