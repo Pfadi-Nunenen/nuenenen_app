d80e1ff7959ebfa90b092c5c8cab6b9842da1e1f
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-10-23 21:34:12 +0200

    Update wolf_page.dart

--- lib/screens/kastenzettel/wolf_page.dart
+++ lib/screens/kastenzettel/wolf_page.dart
@@ -9,108 +9,105 @@ import 'package:nuenenen/models/kastenzettel.dart';
 import 'package:nuenenen/user_info.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class WolfPage extends StatefulWidget{
-	@override
-	_WolfPage createState() => _WolfPage();
+class WolfPage extends StatefulWidget {
+  @override
+  _WolfPage createState() => _WolfPage();
 }
 
-class _WolfPage extends State<WolfPage>{
-	List<Kastenzettel> kastenzettelList = new List();
-	var isLoading = false;
+class _WolfPage extends State<WolfPage> {
+  List<Kastenzettel> kastenzettelList = List();
+  var isLoading = false;
 
-	@override
-	void initState(){
-		super.initState();
-		onRefresh();
-	}
+  @override
+  void initState() {
+    super.initState();
+    onRefresh();
+  }
 
-	Future onRefresh() async{
-		setState(() {
-			isLoading = true;
-		});
+  Future onRefresh() async {
+    setState(() {
+      isLoading = true;
+    });
 
-		print("Lade Daten für den Kastenzettel herunter");
-		kastenzettelList.clear();
-		var URL = "${baseURI}/node/${wolfID}/json";
+    print("Lade Daten für den Kastenzettel herunter");
+    kastenzettelList.clear();
+    var URL = "${baseURI}/node/${wolfID}/json";
 
-		try{
-			await http.get(URL).then((response){
-				var json = jsonDecode(response.body);
-				for(int i = 0; i < json.length; i++) {
-					setState(() {
-						kastenzettelList.add(new Kastenzettel(
-								json['title'].toString(),
-								json['body']['und'][0]['value'].toString()
-						));
-					});
-				}
-			});
-			setState(() {
-				isLoading = false;
-			});
-		}catch(error){
-			print("Failed to get Kastenzettel - $error");
-		}
-	}
+    try {
+      await http.get(URL).then((response) {
+        var json = jsonDecode(response.body);
+        for (int i = 0; i < json.length; i++) {
+          setState(() {
+            kastenzettelList.add(Kastenzettel(
+              json['title'].toString(),
+              json['body']['und'][0]['value'].toString()
+            ));
+          });
+        }
+      });
+      setState(() {
+        isLoading = false;
+      });
+    } catch (error) {
+      print("Failed to get Kastenzettel - $error");
+    }
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: CupertinoNavigationBar(
-				backgroundColor: mainColor,
-				actionsForegroundColor: Colors.white,
-				previousPageTitle: "Stufen",
-				middle: new Text(
-					"Phönix",
-					style: TextStyle(
-							color: Colors.white
-					),
-				),
-			),
-			body: isLoading ?
-			new Container(
-					color: currBackgroundColor,
-					child: Center(
-						child: CircularProgressIndicator(),
-					)
-			)
-					: new RefreshIndicator(
-				onRefresh: onRefresh,
-				color: darkTextColor,
-				backgroundColor: currBackgroundColor,
-				child: new Container(
-					color: currBackgroundColor,
-					padding: EdgeInsets.all(16.0),
-					child: new Container(
-						child: new Column(
-							children: <Widget>[
-								new Text(
-									kastenzettelList[0].title.toString(),
-									style: TextStyle(
-											color: currTextColor,
-											fontSize: 25.0,
-											fontWeight: FontWeight.bold
-									),
-								),
-								new Html(
-									data: kastenzettelList[0].content,
-									style: {
-										"html": Style(whiteSpace: WhiteSpace.PRE),
-										"html": Style.fromTextStyle(TextStyle(color: currTextColor)),
-									},
-									onLinkTap: (url) async {
-										if (await canLaunch(url)) {
-											await launch(url, forceSafariVC: false);
-										} else {
-											throw 'Could not launch $url';
-										}
-									},
-								),
-							],
-						),
-					),
-				),
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        backgroundColor: mainColor,
+        actionsForegroundColor: Colors.white,
+        previousPageTitle: "Stufen",
+        middle: Text(
+          "Phönix",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: isLoading
+          ? Container(
+              color: currBackgroundColor,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ))
+          : RefreshIndicator(
+              onRefresh: onRefresh,
+              color: darkTextColor,
+              backgroundColor: currBackgroundColor,
+              child: Container(
+                color: currBackgroundColor,
+                padding: EdgeInsets.all(16.0),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        kastenzettelList[0].title.toString(),
+                        style: TextStyle(
+                            color: currTextColor,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Html(
+                        data: kastenzettelList[0].content,
+                        style: {
+                          "html": Style(whiteSpace: WhiteSpace.PRE),
+                          "html": Style.fromTextStyle(
+                              TextStyle(color: currTextColor)),
+                        },
+                        onLinkTap: (url) async {
+                          if (await canLaunch(url)) {
+                            await launch(url, forceSafariVC: false);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
 }
