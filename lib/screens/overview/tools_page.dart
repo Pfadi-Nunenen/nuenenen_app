97a356a5117762b7aef61d3756bd7a2d8fbc3cf4
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-10-23 21:37:39 +0200

    Update tools_page.dart

--- lib/screens/overview/tools_page.dart
+++ lib/screens/overview/tools_page.dart
@@ -4,81 +4,99 @@ import 'package:flutter/material.dart';
 import 'package:nuenenen/theme/colors.dart';
 import 'package:nuenenen/user_info.dart';
 
-class ToolsPage extends StatefulWidget{
-	@override
-	_ToolsPage createState() => _ToolsPage();
+class ToolsPage extends StatefulWidget {
+  @override
+  _ToolsPage createState() => _ToolsPage();
 }
 
 class _ToolsPage extends State<ToolsPage> {
-	@override
-	Widget build(BuildContext context) {
-		return NestedScrollView(
-			headerSliverBuilder: (BuildContext context, bool isScrolled) {
-				return [
-					new CupertinoSliverNavigationBar(
-						largeTitle: new Text(
-							"Stufen",
-							style: TextStyle(
-									color: Colors.white
-							),
-						),
-						backgroundColor: mainColor,
-					),
-				];
-			},
-			body: new Container(
-				padding: EdgeInsets.only(top: 16.0, right: 16.0, left: 16.0),
-				color: currCardColor,
-				child: new SingleChildScrollView(
-					child: new Column(
-						children: <Widget>[
-							new Padding(padding: EdgeInsets.all(8.0)),
-							new Card(
-								color: currBackgroundColor,
-								child: Column(
-									crossAxisAlignment: CrossAxisAlignment.start,
-									children: <Widget>[
-										new Container(
-											padding: EdgeInsets.all(16.0),
-											child: new Text("QR Code Reader", style: TextStyle(color: mainColor, fontFamily: "Product Sans", fontWeight: FontWeight.bold),),
-										),
-										new ListTile(
-											title: new Text("QR Code Reader", style: TextStyle(fontFamily: "Product Sans", color: currTextColor)),
-											trailing: new Icon(Icons.navigate_next, color: mainColor),
-											onTap: () {
-												router.navigateTo(context, '/qr-reader', transition: TransitionType.native);
-											},
-										),
-										new Divider(height: 0.0, color: mainColor),
-									],
-								),
-							),
-							new Padding(padding: EdgeInsets.all(8.0)),
-							new Card(
-								color: currBackgroundColor,
-								child: Column(
-									crossAxisAlignment: CrossAxisAlignment.start,
-									children: <Widget>[
-										new Container(
-											padding: EdgeInsets.all(16.0),
-											child: new Text("Digitales Gästebuch", style: TextStyle(color: mainColor, fontWeight: FontWeight.bold, fontFamily: "Product Sans"),),
-										),
-										new ListTile(
-											title: new Text("Digitales Gästebuch", style: TextStyle(fontFamily: "Product Sans", color: currTextColor)),
-											trailing: new Icon(Icons.navigate_next, color: mainColor),
-											onTap: () {
-												router.navigateTo(context, '/guestbook', transition: TransitionType.native);
-											},
-										),
-										new Divider(height: 0.0, color: mainColor),
-									],
-								),
-							),
-							new Padding(padding: EdgeInsets.all(8.0)),
-						],
-					),
-				),
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool isScrolled) {
+        return [
+          CupertinoSliverNavigationBar(
+            largeTitle: Text(
+              "Stufen",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: mainColor,
+          ),
+        ];
+      },
+      body: Container(
+        padding: EdgeInsets.only(top: 16.0, right: 16.0, left: 16.0),
+        color: currCardColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(8.0)),
+              Card(
+                color: currBackgroundColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "QR Code Reader",
+                        style: TextStyle(
+                            color: mainColor,
+                            fontFamily: "Product Sans",
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("QR Code Reader",
+                          style: TextStyle(
+                              fontFamily: "Product Sans",
+                              color: currTextColor)),
+                      trailing: Icon(Icons.navigate_next, color: mainColor),
+                      onTap: () {
+                        router.navigateTo(context, '/qr-reader',
+                            transition: TransitionType.native);
+                      },
+                    ),
+                    Divider(height: 0.0, color: mainColor),
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(8.0)),
+              Card(
+                color: currBackgroundColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "Digitales Gästebuch",
+                        style: TextStyle(
+                            color: mainColor,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Product Sans"),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("Digitales Gästebuch",
+                          style: TextStyle(
+                              fontFamily: "Product Sans",
+                              color: currTextColor)),
+                      trailing: Icon(Icons.navigate_next, color: mainColor),
+                      onTap: () {
+                        router.navigateTo(context, '/guestbook',
+                            transition: TransitionType.native);
+                      },
+                    ),
+                    Divider(height: 0.0, color: mainColor),
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(8.0)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
