ca7ca377cc13ae01fa34069339cca3bce7dafc2e
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-11-03 16:33:08 +0100

    Create app.dart

---
+++ lib/ui/app.dart
@@ -0,0 +1,27 @@
+import 'package:nuenenen_app/constants/strings.dart';
+import 'package:nuenenen_app/router.dart';
+import 'package:nuenenen_app/ui/screens/initial.dart';
+import 'package:nuenenen_app/ui/theme_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return Material(
+      color: Colors.transparent,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppStrings.appName,
+        theme: themeData(context),
+        initialRoute: InitialScreen.routeName,
+        routes: AppRouter.routes,
+      ),
+    );
+  }
+}
