7e7a8f639129b54f44f2a80bbd7c0a6840f13098
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-11-03 17:23:16 +0100

    Update create_playlist_sheet.dart

--- lib/ui/screens/create_playlist_sheet.dart
+++ lib/ui/screens/create_playlist_sheet.dart
@@ -1,11 +1,14 @@
-import 'package:nuenenen_app/constants/colors.dart';
-import 'package:nuenenen_app/constants/dimensions.dart';
-import 'package:nuenenen_app/providers/playlist_provider.dart';
-import 'package:nuenenen_app/ui/widgets/message_overlay.dart';
-import 'package:nuenenen_app/ui/widgets/spinner.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'package:nuenenen_app/constants/colors.dart';
+import 'package:nuenenen_app/constants/dimensions.dart';
+
+import 'package:nuenenen_app/providers/playlist_provider.dart';
+
+import 'package:nuenenen_app/ui/widgets/message_overlay.dart';
+import 'package:nuenenen_app/ui/widgets/spinner.dart';
 
 class CreatePlaylistSheet extends StatefulWidget {
   static final Key nameFieldKey = UniqueKey();
@@ -121,16 +124,16 @@ class _AddPlaylistScreenState extends State<CreatePlaylistSheet> {
               _working
                   ? spinner
                   : ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        minWidth: double.infinity,
-                      ),
-                      child: ElevatedButton(
-                        key: CreatePlaylistSheet.submitButtonKey,
-                        style: ElevatedButton.styleFrom(onSurface: Colors.grey),
-                        onPressed: _enabled ? () async => await submit() : null,
-                        child: const Text('Create Playlist'),
-                      ),
-                    ),
+                constraints: const BoxConstraints(
+                  minWidth: double.infinity,
+                ),
+                child: ElevatedButton(
+                  key: CreatePlaylistSheet.submitButtonKey,
+                  style: ElevatedButton.styleFrom(onSurface: Colors.grey),
+                  onPressed: _enabled ? () async => await submit() : null,
+                  child: const Text('Create Playlist'),
+                ),
+              ),
             ],
           ),
         ),
