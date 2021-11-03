b6ef28e851e281953573b21424a26d8634bb09d9
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-11-03 16:32:17 +0100

    Create auth_provider.dart

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,44 @@
+import 'package:nuenenen_app/models/user.dart';
+import 'package:nuenenen_app/utils/api_request.dart';
+import 'package:nuenenen_app/utils/preferences.dart' as preferences;
+
+class AuthProvider {
+  late User _authUser;
+  User get authUser => _authUser;
+
+  Future<bool> login({required String email, required String password}) async {
+    final Map<String, String> loginData = {
+      'email': email,
+      'password': password,
+    };
+
+    try {
+      final responseData = await post('me', data: loginData);
+      preferences.apiToken = responseData['token'];
+      return true;
+    } catch (err) {
+      print(err);
+      return false;
+    }
+  }
+
+  void setAuthUser(User user) => _authUser = user;
+
+  Future<User?> tryGetAuthUser() async {
+    if (preferences.apiToken == null) {
+      return null;
+    }
+
+    this.setAuthUser(User.fromJson(await get('me')));
+
+    return authUser;
+  }
+
+  Future<void> logout() async {
+    try {
+      await delete('me');
+    } catch (_) {}
+
+    preferences.apiToken = null;
+  }
+}
