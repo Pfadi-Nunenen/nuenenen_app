57f0884b65373d75d538019e56941e1976a25f8c
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-11-03 16:32:03 +0100

    Create album_provider.dart

---
+++ lib/providers/album_provider.dart
@@ -0,0 +1,57 @@
+import 'package:nuenenen_app/models/album.dart';
+import 'package:nuenenen_app/values/parse_result.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+import 'artist_provider.dart';
+
+ParseResult parseAlbums(List<dynamic> data) {
+  ParseResult result = ParseResult();
+  data.forEach((json) => result.add(Album.fromJson(json), json['id']));
+
+  return result;
+}
+
+class AlbumProvider with ChangeNotifier {
+  ArtistProvider _artistProvider;
+  late List<Album> _albums;
+  late Map<int, Album> _index;
+
+  AlbumProvider({required ArtistProvider artistProvider})
+      : _artistProvider = artistProvider;
+
+  List<Album> get albums => _albums;
+
+  Future<void> init(List<dynamic> albumData) async {
+    ParseResult result = await compute(parseAlbums, albumData);
+    _albums = result.collection.cast();
+    _index = result.index.cast();
+
+    _albums.forEach((album) {
+      album.artist = _artistProvider.byId(album.artistId);
+    });
+  }
+
+  Album byId(int id) => _index[id]!;
+
+  List<Album> byIds(List<int> ids) {
+    List<Album> albums = [];
+
+    ids.forEach((id) {
+      if (_index.containsKey(id)) {
+        albums.add(_index[id]!);
+      }
+    });
+
+    return albums;
+  }
+
+  List<Album> mostPlayed({int limit = 15}) {
+    List<Album> clone = List<Album>.from(_albums)
+        .where((album) => album.isStandardAlbum)
+        .toList()
+          ..sort((a, b) => b.playCount.compareTo(a.playCount));
+
+    return clone.take(limit).toList();
+  }
+}
