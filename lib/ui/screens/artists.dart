396815065e6af025e63f56efeb7118a31a9d4e0a
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-11-03 16:33:54 +0100

    Create artists.dart

---
+++ lib/ui/screens/artists.dart
@@ -0,0 +1,73 @@
+import 'package:nuenenen_app/models/artist.dart';
+import 'package:nuenenen_app/providers/artist_provider.dart';
+import 'package:nuenenen_app/router.dart';
+import 'package:nuenenen_app/ui/widgets/artist_thumbnail.dart';
+import 'package:nuenenen_app/ui/widgets/bottom_space.dart';
+import 'package:nuenenen_app/ui/widgets/typography.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ArtistsScreen extends StatefulWidget {
+  static const routeName = '/artists';
+  final AppRouter router;
+
+  const ArtistsScreen({
+    Key? key,
+    this.router = const AppRouter(),
+  }) : super(key: key);
+
+  @override
+  _ArtistsScreenState createState() => _ArtistsScreenState();
+}
+
+class _ArtistsScreenState extends State<ArtistsScreen> {
+  late ArtistProvider artistProvider;
+  late List<Artist> _artists = [];
+
+  @override
+  void initState() {
+    super.initState();
+    artistProvider = context.read();
+    setState(() => _artists = artistProvider.artists);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CupertinoTheme(
+        data: CupertinoThemeData(
+          primaryColor: Colors.white,
+        ),
+        child: CustomScrollView(
+          slivers: [
+            CupertinoSliverNavigationBar(
+              backgroundColor: Colors.black,
+              largeTitle: const LargeTitle(text: 'Artists'),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  Artist artist = _artists[index];
+                  return InkWell(
+                    onTap: () => widget.router.gotoArtistDetailsScreen(
+                      context,
+                      artist: artist,
+                    ),
+                    child: ListTile(
+                      shape: Border(bottom: Divider.createBorderSide(context)),
+                      leading: ArtistThumbnail(artist: artist, asHero: true),
+                      title: Text(artist.name, overflow: TextOverflow.ellipsis),
+                    ),
+                  );
+                },
+                childCount: _artists.length,
+              ),
+            ),
+            const BottomSpace(),
+          ],
+        ),
+      ),
+    );
+  }
+}
