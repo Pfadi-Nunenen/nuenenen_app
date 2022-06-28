fabc0c2a269a2725f14e965be44ef30257e7d7c8
Caspar Brenneisen <github@brenneisen.me>
2022-06-28 18:08:38 +0200

    Update sortable_song_list.dart

--- lib/ui/widgets/sortable_song_list.dart
+++ lib/ui/widgets/sortable_song_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:nuenenen_app/models/song.dart';
+import 'package:nuenenen/models/song.dart';
 
 enum OrderBy {
   trackNumber,
