ad38bda2f21084d5196b50f88e15880caf37c16f
Caspar Brenneisen <github@brenneisen.me>
2022-06-28 18:09:02 +0200

    Update simple_song_list_test.mocks.dart

--- test/ui/widgets/simple_song_list_test.mocks.dart
+++ test/ui/widgets/simple_song_list_test.mocks.dart
@@ -5,8 +5,8 @@
 import 'dart:async' as _i6;
 import 'dart:ui' as _i7;
 
-import 'package:nuenenen_app/models/song.dart' as _i5;
-import 'package:nuenenen_app/providers/audio_provider.dart' as _i4;
+import 'package:nuenenen/models/song.dart' as _i5;
+import 'package:nuenenen/providers/audio_provider.dart' as _i4;
 import 'package:assets_audio_player/assets_audio_player.dart' as _i2;
 import 'package:mockito/mockito.dart' as _i1;
 import 'package:rxdart/rxdart.dart' as _i3;
