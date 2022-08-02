ca44faad3f74d4b945eea5f72e95d5b0f846f9d7
Vento <vento@pfadi-nuenenen.ch>
2022-08-02 19:19:26 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nuenenen_app/main.dart';
+import 'package:nuenenen/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
