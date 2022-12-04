178610c8b2676ca251f5e350faa29d389a7c174c
Vento <github@brenneisen.me>
2022-12-04 21:39:02 +0100

    Update aetna_page.dart

--- lib/screens/kastenzettel/aetna_page.dart
+++ lib/screens/kastenzettel/aetna_page.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:flutter_html/style.dart';
+
 import 'package:http/http.dart' as http;
 import 'package:nuenenen/models/kastenzettel.dart';
 import 'package:nuenenen/theme/colors.dart';
@@ -39,11 +39,13 @@ class _AetnaPageState extends State<AetnaPage> {
         var json = jsonDecode(response.body);
         for(int i = 0; i < json.length; i++) {
           setState(() {
-            kastenzettelList.add(Kastenzettel(
+            kastenzettelList.add(
+              Kastenzettel(
                 json['title'].toString(),
                 json['body']['und'][0]['value'].toString(),
                 json['field_corona_info']['und'][0]['value'].toString()
-            ));
+              )
+            );
           });
         }
       });
@@ -59,7 +61,7 @@ class _AetnaPageState extends State<AetnaPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CupertinoNavigationBar(
+      appBar: const CupertinoNavigationBar(
         backgroundColor: mainColor,
         previousPageTitle: "Stufen",
         middle: Text(
@@ -68,41 +70,44 @@ class _AetnaPageState extends State<AetnaPage> {
         ),
       ),
       body: isLoading
-          ? Container(
+        ? Container(
           color: currBackgroundColor,
-          child: Center(
+          child: const Center(
             child: CircularProgressIndicator(),
-          ))
-          : RefreshIndicator(
-        onRefresh: onRefresh,
-        color: darkTextColor,
-        backgroundColor: currBackgroundColor,
-        child: Container(
-          color: currBackgroundColor,
-          padding: EdgeInsets.all(16.0),
+          )
+        )
+        : RefreshIndicator(
+          onRefresh: onRefresh,
+          color: darkTextColor,
+          backgroundColor: currBackgroundColor,
           child: Container(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  kastenzettelList[0].title.toString(),
-                  style: TextStyle(
+            color: currBackgroundColor,
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    kastenzettelList[0].title.toString(),
+                    style: TextStyle(
                       color: currTextColor,
                       fontSize: 25.0,
                       fontWeight: FontWeight.bold),
-                ),
-                Html(
-                  data: kastenzettelList[0].content,
-                  style: {
-                    "html": Style(
+                  ),
+                  Html(
+                    data: kastenzettelList[0].content,
+                    style: {
+                      "html": Style(
                         whiteSpace: WhiteSpace.PRE,
                         color: currTextColor
                     ),
                   },
-                  onLinkTap: (url) async {
-                    if(await canLaunch(url)) {
-                      await launch(url);
-                    } else {
-                      throw 'Could not launch $url';
+                  onLinkTap: (url, _, __, ___) async {
+                    if(url != null){
+                      if(await canLaunchUrl(Uri.parse(url))) {
+                        await launchUrl(Uri.parse(url));
+                      } else {
+                        throw 'Could not launch $url';
+                      }
                     }
                   },
                 ),
@@ -111,11 +116,13 @@ class _AetnaPageState extends State<AetnaPage> {
                   style: {
                     "html": Style(color: currTextColor),
                   },
-                  onLinkTap: (url) async {
-                    if (await canLaunch(url)) {
-                      await launch(url);
-                    } else {
-                      throw 'Could not launch $url';
+                  onLinkTap: (url, _, __, ___) async {
+                    if(url != null){
+                      if (await canLaunchUrl(Uri.parse(url))) {
+                        await launchUrl(Uri.parse(url));
+                      } else {
+                        throw 'Could not launch $url';
+                      }
                     }
                   },
                 ),
