b1d3db80dc1a2c71ebd6b3609cc35f770c2ca065
Vento <github@brenneisen.me>
2023-12-05 19:39:40 +0100

    Setup ionic

--- lib/theme/theme.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:nuenenen/theme/colors.dart';
-
-ThemeData mainTheme = ThemeData(
-  primaryColor: mainColor,
-  colorScheme: ColorScheme.fromSwatch().copyWith(secondary: mainColor),
-);
---
+++ src/router/index.ts
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import TabsPage from '@/views/TabsPage.vue'
+import HomePage from "@/views/HomePage.vue";
+import StufenPage from "@/views/StufenPage.vue";
+import SettingsPage from "@/views/SettingsPage.vue";
+import BiberPage from "@/views/stufen/BiberPage.vue";
+import WolfPage from "@/views/stufen/WolfPage.vue";
+import PfadiPage from "@/views/stufen/PfadiPage.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/tabs/home'
+  },
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/tab1'
+      },
+      {
+        path: 'home',
+        component: HomePage
+      },
+      {
+        path: 'stufen/',
+        component: StufenPage,
+        children: [
+          {
+            path: 'biber',
+            component: BiberPage,
+          },
+          {
+            path: 'wolf',
+            component: WolfPage
+          },
+          {
+            path: 'pfadi',
+            component: PfadiPage
+          }
+        ]
+      },
+      {
+        path: 'settings',
+        component: SettingsPage
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
