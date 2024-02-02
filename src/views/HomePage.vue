d3aca1c74f969d635ddfc0391c40b3899f8d6460
Vento <github@brenneisen.me>
2024-02-03 00:32:46 +0100

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -28,9 +28,4 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
-
-<style scoped>
-
-</style>
