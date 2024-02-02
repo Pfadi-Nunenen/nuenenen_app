559c9514084324bd3768a93ac67cc74fd99e6d72
Vento <github@brenneisen.me>
2024-02-03 00:57:58 +0100

    Update BiberPage.vue

--- src/views/stufen/BiberPage.vue
+++ src/views/stufen/BiberPage.vue
@@ -1,10 +1,25 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Biber</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Biber</ion-title>
+        </ion-toolbar>
+      </ion-header>
 
+      Biber
+    </ion-content>
+  </ion-page>
 </template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
 
 <style scoped>
 
